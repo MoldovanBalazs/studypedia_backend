607bb514da1b9fb8b5166ee7c8b1238aeaae4f55
ioanmarcu <ioan.marcu@nttdata.ro>
2018-08-14 17:53:00 +0300

    minor refactoring

--- src/main/java/ro/internteam/studypedia/resource/UniversityResource.java
+++ src/main/java/ro/internteam/studypedia/resource/UniversityResource.java
@@ -1,7 +1,13 @@
 package ro.internteam.studypedia.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ro.internteam.studypedia.model.Faculty;
 import ro.internteam.studypedia.model.University;
 import ro.internteam.studypedia.service.FacultyService;
@@ -21,17 +27,19 @@ public class UniversityResource {
     @Autowired
     private FacultyService facultyService;
 
-    /**Adi*/
+    /**
+     * Adi
+     */
     @PostMapping(path = "/insertUniversity")
     public String insertUniversity(
             @RequestBody University newUniversity
-            ){
+    ) {
         List<Faculty> faculties = new ArrayList<>();
-        for(Faculty faculty : newUniversity.getFaculties()){
+        for (Faculty faculty : newUniversity.getFaculties()) {
             faculties.add(faculty);
         }
         University university = this.universityService.saveUniversity(newUniversity);
-        for(Faculty faculty : faculties){
+        for (Faculty faculty : faculties) {
             faculty.setUniversity(newUniversity);
             this.facultyService.insertFaculty(faculty, newUniversity.getId());
         }
@@ -39,21 +47,21 @@ public class UniversityResource {
     }
 
     @GetMapping(path = "/university/{id}")
-    public Object getUniversityById(@PathVariable("id") Integer id){
+    public Object getUniversityById(@PathVariable("id") Integer id) {
         return this.universityService.getUniversityById(id);
     }
 
     @GetMapping(path = "/university/all")
-    public Object getUniversities(@RequestParam("param") String param ){
-
-        if(param == null){return null;}
-        List<University> universities =  new ArrayList<>();
-        for(University university : universityService.getUniversities()){
-            if(university.getName().contains(param)){
+    public Object getUniversities(@RequestParam(value = "param", defaultValue = "default") String param) {
+        if (param.equalsIgnoreCase("default")) {
+            return universityService.getUniversities();
+        }
+        List<University> universities = new ArrayList<>();
+        for (University university : universityService.getUniversities()) {
+            if (university.getName().contains(param)) {
                 universities.add(university);
             }
         }
         return universities;
     }
-
 }
